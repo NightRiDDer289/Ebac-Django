8a8fa7d148f0474542f23885fc09b785154c944d
Vitor Ramos <vitorramos66666@gmail.com>
2026-04-14 10:29:17 -0300

    post django-admin

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,4 +1,13 @@
+
 from django.contrib import admin
 
-
+# Register your models here.
 from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_on')
+    list_filter = ('status', 'created_on', 'author')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Post, PostAdmin)

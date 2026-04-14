4a42eec584b34273c1c89d1a7be7db52fdcb2ac1
Vitor Ramos <vitorramos66666@gmail.com>
2026-04-14 10:24:46 -0300

    add view

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import generic
+
+
+class PostView(generic.View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("Hello, World!")
+
+
 from django.shortcuts import render
 
 # Create your views here.
